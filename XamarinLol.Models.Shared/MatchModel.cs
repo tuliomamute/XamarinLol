68420da75fc5109b2bce0c3e967f16918976cfc9
Túlio Rezende <tulio.mamute@hotmail.com>
2017-03-22 11:45:27 -0300

    linkagem de arquivos

--- XamarinLol.Models.Shared/MatchModel.cs
+++ XamarinLol.Models.Shared/MatchModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 
-namespace XamarinLoL.Models.Shared
+namespace XamarinLoL.Models
 {
     public class MatchModel
     {
